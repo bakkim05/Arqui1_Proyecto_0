1481d6e7c46c55f3c911a0c60b84eb2f523b5d49
Jung <bakkim05@gmail.com>
2020-05-24 13:33:30 -0600

    matrix and size file routing corrected

--- imgConvert.py
+++ imgConvert.py
@@ -4,11 +4,40 @@ from PIL import Image as img
 
 def blackBox(imagePath):
     photo = img.open(imagePath)
+    name = photo.filename
+    width,height = photo.size
+
     photo = photo.convert("L")
-    print(photo)
+
+    
     y = np.asarray(photo.getdata(),dtype=np.float64).reshape((photo.size[1],photo.size[0]))
-    y = np.asarray(y,dtype=np.uint8) #if values still in range 0-255! 
-
+    y = np.asarray(y,dtype=np.uint8)
+    
     z = y.reshape(1,y.size)
+    
+    widthFixerAndPrinter(z[0],name,width,height)
 
-    np.savetxt("data.txt",z,delimiter=',',fmt='%d')
+
+
+def widthFixerAndPrinter(matrix,filename,width,height):
+    output = ''
+
+    if filename[-4:] == 'jpeg':
+        filename = filename[:-5]
+    else:
+        filename = filename[:-4]
+
+    size = str(width).zfill(5)+str(height).zfill(5)
+    
+    for i in matrix:
+        output += str(i).zfill(3)
+
+    with open(filename+"_matrix.txt","w") as text_file:
+        print(output, file=text_file)
+        
+    with open(filename+"_size.txt","w") as text_file:
+        print(size,file=text_file)
+
+    
+
+blackBox("images/pngMade.png")
